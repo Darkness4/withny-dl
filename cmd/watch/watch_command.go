5506f48e921bd033b8ba050601d515f86445c45a
Nguyen Marc <nguyen_marc@live.fr>
2024-09-08 00:00:19 +0200

    fix(watch): dont use same context for panic

--- cmd/watch/watch_command.go
+++ cmd/watch/watch_command.go
@@ -217,6 +217,8 @@ func handleConfig(ctx context.Context, version string, config *Config) {
 	defer func() {
 		if err := recover(); err != nil {
 			fmt.Println(err)
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+			defer cancel()
 			if err := notifier.NotifyPanicked(ctx, err); err != nil {
 				log.Err(err).Msg("notify failed")
 			}
